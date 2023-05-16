0c2a402682aafdb7aec179305d9919d29cbfcbd0
Ionela Pascaru <ipascaru@c.fitbit.com>
2023-05-16 15:34:50 +0300

    gig backend

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -1,3 +1,65 @@
-export const fn = (req, res) => {
-    res.send("controller");
-}
+import Gig from "../models/gig.model.js";
+import createError from "../utils/createError.js";
+
+export const createGig = async (req, res, next) => {
+    if (!req.isSeller)
+        return next(createError(403, "Only sellers can create gig!"));
+    const newGig = new Gig({
+        userId: req.userId,
+        ...req.body,
+    });
+
+    try {
+        const savedGig = await newGig.save();
+        res.status(201).json(savedGig);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const deleteGig = async (req, res, next) => {
+    try {
+        const gig = await Gig.findById(req.params.id);
+        if (gig.userId !== req.userId)
+            return next(createError(403, "You can delete only your gig!"));
+
+        await Gig.findByIdAndDelete(req.params.id);
+        res.status(200).send("Gig has been deleted!");
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getGig = async (req, res, next) => {
+    try {
+        const gig = await Gig.findById(req.params.id);
+        if (!gig) return next(createError(404, "Gig not found!"));
+        res.status(200).send(gig);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getGigs = async (req, res, next) => {
+    const q = req.query;
+    const filters = {
+        ...(q.userId && { userId: q.userId }),
+        ...(q.cat && { cat: q.cat }), //(... spread operator) if exists cat add to our obj
+        ...((q.min || q.max) && {
+            price: {
+                ...(q.min && { $gt: q.min }),
+                ...(q.max && { $lt: q.max }),
+            },
+        }), //$gt => grater than x. $lt => less than x
+        ...(q.search && {title: {
+            $regex: q.search,
+            $options: "i" /*not sesitive upper or lowe case */,
+        }}),
+    };
+    try {
+        const gigs = await Gig.find(filters);
+        res.status(200).send(gigs);
+    } catch (err) {
+        next(err);
+    }
+};
